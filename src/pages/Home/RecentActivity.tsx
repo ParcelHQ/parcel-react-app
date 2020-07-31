bf2239dc1ca722bb3bf80aac823c294a47bec4b3
Brennan Fife <brenfife@gmail.com>
2020-07-31 15:10:12 -0500

    commit

--- src/pages/Home/RecentActivity.tsx
+++ src/pages/Home/RecentActivity.tsx
@@ -22,15 +22,15 @@ export default function RecentActivity() {
         <tbody>
           <tr>
             <td>28/07/2018</td>
+            <td>$2500</td>
             <td>Brennan.eth</td>
             <td>Paid Contractor Brennan Fife $545 for 'animation platfrom'</td>
-            <td>$2500</td>
           </tr>
           <tr>
             <td>28/07/2018</td>
+            <td>$2500</td>
             <td>John.eth</td>
             <td>John Smith is requesting $324 for 'marketing campaign'</td>
-            <td>$2500</td>
           </tr>
         </tbody>
       </Table>
